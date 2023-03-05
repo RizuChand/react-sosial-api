6d90f9c0619bfe898b6412443adb9e33ab865d1f
Chandra Rizaldy <dragoriza123@gmail.com>
2023-03-05 12:57:13 +0700

    register function

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,32 @@
 const Users = require("../models/Users");
+const bcrypt = require("bcrypt");
 
 const router = require("express").Router();
 
-router.get("/register", async (req,res)=>{
 
-    const user = await new Users({
-        username : "lovharem",
-        email : "harem@gmail.com",
-        password : "yahooo"
-    })
-    await user.save();
-    res.send("ini masuk ke router auth hello world")
+// REGISTER
+
+router.post("/register", async (req,res)=>{
+    try {
+        // generate password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
+        //new user
+        const newUser = new Users({
+        username : req.body.username,
+        email : req.body.email,
+        password : hashedPass,
+        }) 
+        //save new user
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        console.log("error disini");
+        
+    }
 })
+
+// LOGIN
 
 
 
