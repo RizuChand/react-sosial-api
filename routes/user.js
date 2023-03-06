a86f1a1ffdfb267ce9d25242c2c951e981a0aca6
Chandra Rizaldy <dragoriza123@gmail.com>
2023-03-06 17:10:08 +0700

    update ,delete and get

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const Users = require("../models/Users")
 //update user
 router.put("/:id", async (req, res)=>{
-    if(req.body.userId === req.params.id || req.user.isAdmin) {
+    if(req.body.userId === req.params.id || req.body.isAdmin) {
         if(req.body.password){
             try {
                 const salt = await bcrypt.genSalt(10);
@@ -26,12 +26,32 @@ router.put("/:id", async (req, res)=>{
     }
 })
 
-
 //delete user
-
+router.delete("/:id", async (req, res)=>{
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            const user = await Users.findByIdAndDelete(req.params.id);
+            res.status(200).json("account has been deleted");
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        return res.status(403).json("you can only delete yourself account!")
+    }
+})
 
 //get user
+router.get("/:id", async (req, res)=>{
+    try {
+        const user = await Users.findById(req.params.id);
+        const {password , updatedAt, ...other} = user._doc
+        res.status(200).json(other);
+    } catch (error) {
+        res.status(404).json(error);
+        
+    }
 
+})
 
 //follow user
 
